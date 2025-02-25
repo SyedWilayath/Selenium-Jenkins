712b56f052bd42a3c827a5ac1bb4e8f88d8b7e8d
Syed Wilayath Khadari <syedwilayath2@gmail.com>
2025-02-26 00:32:53 +0530

    Update LoginBasicTest.java

--- src/test/java/tests/LoginBasicTest.java
+++ src/test/java/tests/LoginBasicTest.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-// import org.openqa.selenium.support.ui.WebDriverWait;
-// import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
